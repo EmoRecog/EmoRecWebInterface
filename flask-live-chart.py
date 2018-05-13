10b246d7854e7fb67b5d23921594c0e51d38774f
Shubham Punekar <srp3003@gmail.com>
2018-05-13 19:13:03 +0530

    beautiful commit

--- flask-live-chart.py
+++ flask-live-chart.py
@@ -32,4 +32,5 @@ def live_data():
     response.content_type = 'application/json'
     return response
 
-app.run(debug=True, host='127.0.0.1', port=5000)
+# app.run(debug=True, host='127.0.0.1', port=5000)
+app.run(debug=True, host='0.0.0.0', port=5000)
